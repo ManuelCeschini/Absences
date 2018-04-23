66befc7fa0d72b2afca8de10f7a0869ea1f7e11a
ManuelCeschini <Manuel.Ceschini@gmail.com>
2018-04-23 21:06:11 +0200

    Changes: MainActivity.java Register.java misc.xml activity_main.xml activity_register.xml

--- app/src/main/java/com/example/liquidsoftware/absences/Register.java
+++ app/src/main/java/com/example/liquidsoftware/absences/Register.java
@@ -1,16 +1,23 @@
 package com.example.liquidsoftware.absences;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.TextHttpResponseHandler;
+
+import java.util.Calendar;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -21,7 +28,7 @@ public class Register extends AppCompatActivity {
 
     private EditText name;
     private EditText nachname;
-    private EditText geburtsdatum;
+    private TextView geburtsdatum;
     private EditText email;
     private EditText password;
     private EditText RePassword;
@@ -45,6 +52,46 @@ public class Register extends AppCompatActivity {
         password = findViewById(R.id.re_password);
         RePassword = findViewById(R.id.re_password_wiederholung);
         register();
+        gebDatum();
+    }
+    public void gebDatum(){
+        geburtsdatum.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Calendar mcurrentDate = Calendar.getInstance();
+                int day = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+                int month = mcurrentDate.get(Calendar.MONTH);
+                int year = mcurrentDate.get(Calendar.YEAR);
+                DatePickerDialog mDatePicker;
+                mDatePicker = new DatePickerDialog(Register.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        int[] params = new int[3];
+                        params[0] = String.valueOf(i).length();
+                        params[1] = String.valueOf(i1).length();
+                        params[2] = String.valueOf(i2).length();
+
+                        String z0 = "";
+                        String z1 = "";
+                        String z2 = "";
+
+                        if (params[0] == 1){
+                            z0 = "0";
+                        }
+                        if (params[1] == 1){
+                            z1 = "0";
+                        }
+                        if (params[2] == 1){
+                            z2 = "0";
+                        }
+                        i1 = i1 + 1;
+                        geburtsdatum.setText(z2 + i2 + "/" + z1 + i1 + "/" + z0 + i);
+                    }
+                }, year, month, day);
+                mDatePicker.show();
+            }
+        });
+
+
     }
 
     public void register() {
@@ -57,6 +104,7 @@ public class Register extends AppCompatActivity {
                 emailString = email.getText().toString();
                 PasswordString = password.getText().toString();
                 rePasswordString = RePassword.getText().toString();
+
 
                 //übergabe Parameter
                 // Überprüfe ob es email schon gibt
